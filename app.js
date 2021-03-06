3bcd91a18c76cd8e95c1deffcdb6243ddd49bb41
Jeff Mead <jeffdmead171@gmail.com>
2021-03-06 17:40:53 -0500

    added report items

--- app.js
+++ app.js
@@ -1,12 +1,83 @@
+const inquirer = require('inquirer');
+
 const fs = require('fs');
 const generatePage = require('./src/page-template.js');
 
-const profileDataArg = process.argv.slice(2);
-const [firstN, lastN, addr, cty, state, zp]  = profileDataArg;
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'yourname',
+            message: 'What is your name?'
+        },
+        {
+            type: 'input',
+            name: 'address',
+            message: 'What is your address?'
+        },
+        {
+            type: 'input',
+            name: 'cityState',
+            message: 'What is your city, state?'
+        },
+        {
+            type: 'input',
+            name: 'zip',
+            message: 'What is your zip code?'
+        }
+    ]);
+};
 
 
-fs.writeFile('./dist/index.html', generatePage(firstN, lastN, addr, cty, state, zp), err => {
-    if (err) throw err;
+const promptReport = () => {
+    console.log(`
 
-    console.log('The file was created!')
-});
+    ***************************
+    *    Add Report Items     *
+    *                         *
+    * *************************
+    `);
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'kitchen',
+            message: 'List the items in the kitchen?'
+        },
+        {
+            type: 'input',
+            name: 'bathroom',
+            message: 'List the items in the bathroom?'
+        },
+        {
+            type: 'input',
+            name: 'familyroom',
+            message: 'list items in the family room?'
+        },
+        {
+            type: 'checkbox',
+            name: 'typeHouse',
+            message: 'What type of house?',
+            choices: ['Oakmont', 'Devon', 'Ashford', 'Lake Ridge I', 'Lake Ridge II', 'Dorchester']
+        }
+    ]);
+};
+
+const writeFile = () => {
+    const pageHTML = generatePage(yourname, address, zip, kitchen, bathroom, familyroom, typeHouse);
+
+    fs.writeFile('./dist/index.html', pageHTML, err => {
+        if (err) throw err;
+    
+        console.log('The file was created!')
+    })
+};
+
+promptUser()
+    .then(promptReport)
+    .then(writeFile);
+    
+
+
+// process.argv.slice(2) gets input from the terminal
+// const profileDataArg = process.argv.slice(2);
+// const [firstN, lastN, addr, cty, state, zp]  = profileDataArg;
